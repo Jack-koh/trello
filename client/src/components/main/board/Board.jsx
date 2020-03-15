f591a41c4a9773b0f24f0265987c8a2383c583d0
Jack-koh <58971165+Jack-koh@users.noreply.github.com>
2020-03-16 00:36:20 +0900

    boardList grid system 조정(미디어쿼리)

--- client/src/components/main/board/Board.jsx
+++ client/src/components/main/board/Board.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { MdPersonOutline } from 'react-icons/md'
 import './Board.scss'
 import BoardList from './boardList/BoardList'
 import CreateBoard from './createBoard/CreateBoard'
 
-function Board(props) {
-  const { boardList } = props
+function Board() {
   return (
     <section className="board_wrap">
       <div className="board_inner_area">
@@ -15,7 +13,7 @@ function Board(props) {
             <MdPersonOutline />
             <span>Personal Boards</span>
           </div>
-          <ul className={`board_item_section count_${boardList.length}`}>
+          <ul className="board_item_section">
             <BoardList />
             <CreateBoard />
           </ul>
@@ -25,10 +23,4 @@ function Board(props) {
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    boardList: state.board.list
-  }
-}
-
-export default connect(mapStateToProps)(Board)
+export default Board
