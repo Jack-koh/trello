e0f6fef3ed2e7b4ee67c854dca9dba6da3674b4b
koh <rhgusdud09@naver.com>
2020-03-25 17:09:43 +0900

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import axios from 'axios'
-import Router from 'router/Router'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import axios from 'axios';
+import Router from 'router/Router';
+import { BrowserRouter } from 'react-router-dom';
 
-axios.defaults.baseURL = '/api/'
+axios.defaults.baseURL = '/api/';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Router />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/trello/trelloItem/createCard/CreateCard.jsx
+++ client/src/components/trello/trelloItem/createCard/CreateCard.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import * as action from 'store/actions'
-import { MdClose } from 'react-icons/md'
-import { Button, Textarea, ClickOutside } from 'components/custom/Elements'
-import './CreateCard.scss'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as action from 'store/actions';
+import { MdClose } from 'react-icons/md';
+import { Button, Textarea, ClickOutside } from 'components/custom/Elements';
+import './CreateCard.scss';
 
 function CardAddForm(props) {
-  const card = useSelector(state => state.card)
-  const dispatch = useDispatch()
-  const onCreateCard = payload => dispatch(action.createCardStart(payload))
+  const card = useSelector((state) => state.card);
+  const dispatch = useDispatch();
+  const onCreateCard = (payload) => dispatch(action.createCardStart(payload));
 
-  const { trelloItem, closeHandler } = props
-  const [title, setTitle] = useState('')
+  const { trelloItem, closeHandler } = props;
+  const [title, setTitle] = useState('');
 
-  const autosizeHandler = e => {
-    setTitle(e.target.value)
+  const autosizeHandler = (e) => {
+    setTitle(e.target.value);
     // 스크롤 높이값만큼 오브젝트 높이를 맞춰준다.
-    e.target.style.cssText = 'height:5.7rem;'
-    e.target.style.cssText = `height: ${e.target.scrollHeight / 10}rem`
-  }
+    e.target.style.cssText = 'height:5.7rem;';
+    e.target.style.cssText = `height: ${e.target.scrollHeight / 10}rem`;
+  };
 
-  const createCardSubmit = e => {
-    e.preventDefault()
+  const createCardSubmit = (e) => {
+    e.preventDefault();
     if (title.length) {
-      onCreateCard({ trelloId: trelloItem._id, title })
-      setTitle('')
+      onCreateCard({ trelloId: trelloItem._id, title });
+      setTitle('');
     }
-  }
+  };
 
   return (
     <ClickOutside className="card_form_field" close={closeHandler}>
@@ -37,7 +37,7 @@ function CardAddForm(props) {
             type="text"
             placeholder="Enter a title for this card"
             autoFocus
-            onChange={e => autosizeHandler(e)}
+            onChange={(e) => autosizeHandler(e)}
           />
         </div>
         <div className="card_add_control">
@@ -46,7 +46,7 @@ function CardAddForm(props) {
         </div>
       </form>
     </ClickOutside>
-  )
+  );
 }
 
-export default CardAddForm
+export default CardAddForm;
