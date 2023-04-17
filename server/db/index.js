3777bab3599ed971f3e38cfffeb088dbdef3d55d
Jack-koh <58971165+Jack-koh@users.noreply.github.com>
2023-04-17 20:33:31 +0900

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'trello',
-  password: 'Yerin12#',
+  database: '',
+  password: '',
   port: 5432,
 })
 
